c2d4c663f07dc87687fcb3c792c6f4a31dad366f
Maple Lazuli <ada.lazuli@spaceforce.mil>
2024-02-11 01:18:52 -0500

    added second button

---
+++ 02-Simple_Button/app.py
@@ -0,0 +1,18 @@
+# Signals and slots are used for communication between widgets
+import sys
+from PySide6.QtWidgets import QApplication, QPushButton
+from PySide6.QtCore import Slot
+
+@Slot() #required to avoid bugs
+def say_hello():
+    print("Button Clicked, Hello!")
+
+app = QApplication(sys.argv)
+
+button = QPushButton("Click Me")
+
+button.clicked.connect(say_hello)
+
+button.show()
+
+app.exec()
